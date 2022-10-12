f1b2ef7ce38f395564eaab3fe1e673398b26c18b
CristoferSilva <gabrielc2828@gmail.com>
2022-10-12 13:11:52 -0300

    [OLPO-28]: Adding database files and database folders.

---
+++ MedCare.Professional/databse/ProfessionalDatabse.cs
@@ -0,0 +1,20 @@
+﻿using MedCare.Commons;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using ProfessionalEntity = MedCare.Commons.entities.Professional;
+
+namespace MedCare.Professional.databse
+{
+    public class ProfessionalDatabse : DbContext
+    {
+        public DbSet<ProfessionalEntity> Professionals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite($"DataSource={Path.Combine(ApplicationData.Current.LocalFolder.Path, EnumUsefulStrings.ProfessionalDatabase)}");
+    }
+}
