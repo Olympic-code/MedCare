78edf081cfad63fdd9346f9a5e6b87612cf6253b
Aquiles Rodrigues <aquiles.2020131298@unicap.br>
2022-10-23 18:05:50 -0300

    [OLPO-26]: Create login Authentication Service, Authentication Exceptions, EnumUserType and Unit Tests project.

---
+++ MedCare.UnitTests/Services/AuthenticationServiceTest.cs
@@ -0,0 +1,192 @@
+﻿using MedCare.Commons.Entities;
+using MedCare.DB;
+using MedCare.DB.Services;
+using MedCare.Exceptions;
+using MedCare.Services.AuthenticationServices;
+using MedCare.Services.AutheticationServices;
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace MedCare.UnitTests.Services
+{
+    public class AuthenticationServiceTest : IDisposable
+    {
+
+        private PatientRepository patientRepository;
+        private ProfessionalRepository professionalRepository;
+        private IAuthenticationService authenticationService;
+
+        public AuthenticationServiceTest()
+        {
+            DatabasesConfiguration.RunInitialConfiguration();
+            authenticationService = new AuthenticationService();
+            patientRepository = new PatientRepository();
+            professionalRepository = new ProfessionalRepository();
+        }
+
+        [Fact]
+        public async void PatientLoginWithoutAnExistingAccount()
+        {
+
+            try
+            {
+                Patient inexistentPatient = new Patient();
+                await authenticationService.ValidateLogin("patient1@gmail.com", "123456");
+                Assert.True(false);
+                //throw new XunitException();
+            }
+            catch (NotFoundUserException e)
+            {
+                Assert.True(true);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false); 
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        [Fact]
+        public async void PatientLoginWithAnIncorrectPassword()
+        {
+
+            try
+            {
+                Patient patient = new Patient()
+                {
+                    Email = "patient2@gmail.com",
+                    Password = "123456"
+                };
+                await patientRepository.AddNewPatient(patient);
+                await authenticationService.ValidateLogin("patient2@gmail.com", "654321");
+                await patientRepository.RemovePatient(patient);
+                Assert.True(false);
+            }
+            catch (IncorrectPasswordException e)
+            {
+                Assert.True(true);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false);
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        [Fact]
+        public async void PatientLoginWithAValidPassword()
+        {
+
+            try
+            {
+                Patient patient = new Patient()
+                {
+                    Email = "patient3@gmail.com",
+                    Password = "123456"
+                };
+                await patientRepository.AddNewPatient(patient);
+                await authenticationService.ValidateLogin("patient3@gmail.com", "123456");
+                await patientRepository.RemovePatient(patient);
+                Assert.True(true);
+            }
+            catch (IncorrectPasswordException e)
+            {
+                Assert.True(false);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false);
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        [Fact]
+        public async void ProfessionalLoginWithoutAnExistingAccount()
+        {
+
+            try
+            {
+                Professional inexistentProfessional = new Professional();
+                await authenticationService.ValidateLogin("professional1@gmail.com", "abcdef");
+                Assert.True(false);
+                //throw new XunitException();
+            }
+            catch (NotFoundUserException e)
+            {
+                Assert.True(true);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false);
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        [Fact]
+        public async void ProfessionalLoginWithAnIncorrectPassword()
+        {
+
+            try
+            {
+                Professional professional = new Professional()
+                {
+                    Email = "professional2@gmail.com",
+                    Password = "abcdef"
+                };
+                await professionalRepository.AddNewProfessional(professional);
+                await authenticationService.ValidateLogin("professional2@gmail.com", "fedcba");
+                await professionalRepository.RemoveProfessional(professional);
+                Assert.True(false);
+            }
+            catch (IncorrectPasswordException e)
+            {
+                Assert.True(true);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false);
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        [Fact]
+        public async void ProfessionalLoginWithAValidPassword()
+        {
+
+            try
+            {
+                Professional professional = new Professional()
+                {
+                    Email = "professional3@gmail.com",
+                    Password = "abcdef"
+                };
+                await professionalRepository.AddNewProfessional(professional);
+                Tuple<EnumUserType, int> pResult = await authenticationService.ValidateLogin("professional3@gmail.com", "abcdef");
+                await professionalRepository.RemoveProfessional(professional);
+                Assert.True(true);
+            }
+            catch (IncorrectPasswordException e)
+            {
+                Assert.True(false);
+
+            }
+            catch (Exception e)
+            {
+                Assert.True(false);
+                Console.WriteLine("Entity framework error!");
+            }
+        }
+
+        public void Dispose()
+        {
+            //clear actions
+        }
+
+    }
+}
---
+++ MedCare/Services/AuthenticationService/AuthenticationService.cs
@@ -0,0 +1,65 @@
+﻿using MedCare.Commons.Entities;
+using MedCare.DB.Services;
+using MedCare.Exceptions;
+using MedCare.Services.AuthenticationServices;
+using System;
+using System.Security;
+using System.Threading.Tasks;
+
+namespace MedCare.Services.AutheticationServices
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private IPatientRepository patientRepository;
+        private IProfessionalRepository professionalRepository;
+
+        public AuthenticationService()
+        {
+            this.patientRepository = new PatientRepository();
+            this.professionalRepository = new ProfessionalRepository();
+        }
+
+        public async Task<Tuple<EnumUserType, int>> ValidateLogin (string email, String password)
+        {
+
+            Patient patient = new Patient()
+            {
+                Email = email,
+                Password = password
+            };
+
+            Patient patientResult = await patientRepository.GetPatient(patient);
+            if(patientResult != null)
+            {
+                if(patientResult.Password == password)
+                {
+                    return new Tuple<EnumUserType, int>(EnumUserType.PATIENT, patientResult.Id);
+                } else
+                {
+                    throw new IncorrectPasswordException();
+                }
+            }
+
+            Professional professional = new Professional()
+            {
+                Email = email,
+                Password = password
+            };
+
+            Professional professionalResult = await professionalRepository.GetProfessional(professional);
+            if (professionalResult != null)
+            {
+                if (professionalResult.Password == password)
+                {
+                    return new Tuple<EnumUserType, int>(EnumUserType.PROFESSIONAL, professionalResult.Id);
+                } else
+                {
+                    throw new IncorrectPasswordException();
+                }
+            }
+
+            throw new NotFoundUserException();
+        }
+
+    }
+}
