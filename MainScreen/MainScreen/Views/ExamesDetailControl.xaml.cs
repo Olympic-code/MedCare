29b35b15f6107171f42d8aafc1e4f27e2c3612ee
Bruno Martinns <martiins.bruno.97@gmail.com>
2022-11-12 23:54:21 -0300

    Insertion of MainScreen folder

---
+++ MainScreen/MainScreen/Views/ExamesDetailControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MainScreen.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MainScreen.Views
+{
+    public sealed partial class ExamesDetailControl : UserControl
+    {
+        public SampleOrder SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty) as SampleOrder; }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register(nameof(SelectedItem), typeof(SampleOrder), typeof(ExamesDetailControl), new PropertyMetadata(null, OnSelectedItemPropertyChanged));
+
+        public ExamesDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnSelectedItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as ExamesDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
---
+++ MainScreen/MainScreen/Views/ExamesListItemControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using MainScreen.Core.Models;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MainScreen.Views
+{
+    public sealed partial class ExamesListItemControl : UserControl
+    {
+        public SampleOrder Item
+        {
+            get { return (SampleOrder)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemProperty = DependencyProperty.Register(nameof(Item), typeof(SampleOrder), typeof(ExamesListItemControl), new PropertyMetadata(null, OnItemPropertyChanged));
+
+        public ExamesListItemControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
